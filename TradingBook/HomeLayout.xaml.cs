410c758f5238bd2c4ceb750386d95347cbbeb54e
Sofiane Bouragba Zrif <sofiane.zrif@gmail.com>
2018-01-24 23:07:12 +0100

    First Commit + Home layout

---
+++ TradingBook/HomeLayout.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TradingBook
+{
+    /// <summary>
+    /// Logique d'interaction pour HomeLayout.xaml
+    /// </summary>
+    public partial class HomeLayout : UserControl
+    {
+        public HomeLayout()
+        {
+            InitializeComponent();
+            statusLabel.Content = "Welcome back Sofiane ! \n Your total revenue for this month so far is : 15.456£";
+        }
+    }
+}
